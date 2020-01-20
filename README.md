f1604329b52da814efa1f43a7b9b304305b64078
undeadway <undeadway@163.com>
2020-01-20 12:44:35 +0800

    修改描述

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Chinese
-一个基于 Node.js的数据库接口适配器。  
+一个基于 Node.js 的数据库接口适配器。  
 与数据库无关的接口库，用于弥合各种不同的数据库驱动的语法造成的使用差异。  
 使用的时候只要：
 
@@ -15,14 +15,15 @@ db.getList({
 
 最后用 db 变量来调用定义好的方法，如：
 ```
-db.getList(); // 这个list 方法定义在 sql.json 中
+db.getList(); // 这个 list 方法定义在 sql.json 中
 ```
-`bluewater`只负责将 getList 要进行的是哪种操作（增删改查）、所要操作的sql 等  
+`bluewater` 只负责将 getList 要进行的是哪种操作（增删改查）、所要操作的sql 等  
 映射到各个数据库驱动中去，但 如何实现 getList  
 实际的操作依然还是交给各个数据库驱动自己去实现
 
 ## 使用
-因为`bluewater`是数据库驱动的中介，所以本身并不包含数据库驱动。
+**因为使用了 `await` ，所以请注意 nodejs 的版本。**  
+`bluewater` 是数据库驱动的中介，本身并不包含数据库驱动。
 支持的数据库驱动如下表：
 
 | 名称 | 驱动地址 |
@@ -31,24 +32,24 @@ db.getList(); // 这个list 方法定义在 sql.json 中
 | postgres | https://github.com/brianc/node-postgres |
 
 ### 预编译
-`bluewater`所有的sql都是 prepare级别的。
-因为`?`语法一旦数据较多，很容易搞不清楚谁是谁，所以`bluewater`提供了类似
+`bluewater` 所有的 sql 都是 prepare 级别的。  
+同时，因为 `?` 的预编译语法一旦数据较多，很容易搞不清楚谁是谁，所以 `bluewater` 提供了类似
 ```
 SELECT * FROM TABLE WHERE ID = #[id]
 ```
-这种形式，交由`bluewater`编译成通用SQL。
+这种形式，交由 `bluewater` 编译成通用SQL。
 
 ### 查询和修改
-支持`insert`、`delete`、`update`、`select`等基本操作，存储过程（proceduer）、自定义函数（function）等要看所对应的数据库是否支持而定。
+支持`insert` 、`delete` 、`update` 、`select` 等基本操作，存储过程（proceduer）、自定义函数（function）等要看所对应的数据库是否支持而定。
 
 ### 事务
-事务靠一连串的sql拼接而成，所以专门提供一个`transaction`函数来执行。
+事务靠一连串的sql拼接而成，所以专门提供一个 `transaction` 函数来执行。
 ```
 db.transaction([...]);
 ```
 
 ### 使用方式
-将`res`文件夹复制到**项目根目录**下，打开`bluewater.json`可以看到以下内容。
+将 `res` 文件夹复制到**项目根目录**下，打开 `bluewater.json` 可以看到以下内容。
 ```
 {
 	"database-name": "", // 这是数据库类型，写上所要使用的数据库驱动名
@@ -64,17 +65,17 @@ db.transaction([...]);
 }
 ```
 
-按照以上信息配置`bluewater.json`，需要按安丘配置数据库类型（database-name）、连接信息等。
+按照以上信息配置 `bluewater.json` ，需要按安丘配置数据库类型（database-name）、连接信息等。
 
 在使用时， 通过使用：
 ```
 let db = bluewater();
 ```
-来创建`bluewater`的实例，外部调用不用知道内部逻辑，全部交给各个数据库的驱动自己来实现。  
-在实例创建之后，只要通过 其实例调用在`sql.json`中定义好的方法就可以了。
+来创建 `bluewater` 的实例，外部调用不用知道内部逻辑，全部交给各个数据库的驱动自己来实现。  
+在实例创建之后，只要通过 其实例调用在 `sql.json` 中定义好的方法就可以了。
 
 ### sql.json
-在`bluewater.json`的同目录下建立一个`sql.json`的文件，用于记录所有的sql。
+在 `bluewater.json` 的同目录下建立一个 `sql.json` 的文件，用于记录所有的sql。
 
 #### 调用格式
 ```
@@ -96,15 +97,15 @@ db.query({
 	}
 });
 ```
-sql中的`#[id]`和之后代码中的`id`对应。
+sql中的 `#[id]` 和之后代码中的 `id` 对应。
 
 #### 高级格式
-因为可能存在不同条件下，需要对sql加入某些条件变化，所以`bluewater`也支持在sql中添加条件参数。  
+因为可能存在不同条件下，需要对sql加入某些条件变化，所以 `bluewater` 也支持在sql中添加条件参数。  
 如，查看通知，有`过期`和`未过期`两个选项，写出的sql大概如下：
 ```
 SELECT * FROM NOTICE ?[inTime] ?[outTime]
 ```
-在`bluewater.json`中则表现为：
+在 `bluewater.json` 中则表现为：
 ```
 {
 	"listNotice" : {
@@ -119,7 +120,7 @@ SELECT * FROM NOTICE ?[inTime] ?[outTime]
 ```
 
 ### 方法式调用
-如果`bluewater.json`中的`method-query`配置为`ON`，就可以使用方法式调用。  
+如果 `bluewater.json` 中的 `method-query` 配置为 `ON` ，就可以使用方法式调用。  
 一般情况下，SQL的调用是用
 ```
 db.query({
@@ -139,14 +140,14 @@ db.getList({
 	}
 });
 ```
-**方法式调用不默认开启，必须将`bluewater.json`中的`method-query`配置为`ON`才能使用**。
+**方法式调用不默认开启，必须将 `bluewater.json` 中的 `method-query` 配置为 `ON` 才能使用**。
 
 #### 参数中带参
 执行插入语句的时候，因为肯能会带参，出现类似下面的sql：
 ```
 INSER INTO TABLE(PK_COLUMN ?[hasColumn(name)]) VALUES(123 ?[hasColumn(value)])
 ```
-而`sql.json`的配置则是
+而 `sql.json` 的配置则是
 ```
 {
 	"listNotice" : {
--- src/index.js
+++ src/index.js
@@ -1,1 +1,242 @@
-module.exports = exports = require("./bluewater");
+/*
+ * 一个基于 Node.js的数据库接口适配器
+ * 与数据库无关的接口库，用于弥合各种不同的数据库驱动的语法造成的差异
+ * 使用的时候只要 
+ * let sql = require("bluewater");
+ * let db = bluewater();
+ * 就能获得 bluewater 的引用
+ * 
+ * 最后用 db 变量来调用定义好的方法，如：
+ * db.getList(); // 这个list 方法定义在 sql.json 中
+ * bluewater只负责将 getList 要进行的是哪种操作（增删改查）、所要操作的sql 等
+ * 映射到各个数据库驱动中去，但 如何实现 getList
+ * 实际的操作依然还是交给各个数据库驱动自己去实现
+ */
+
+/*
+* 现在所有 sql 都定义在一个大对象下， sql 不多的时候可以这么搞，
+* 今后sql多了以后，要考虑将这些 sql 拆成多个子对象（命名空间概念）分别加载
+* 类似 mybatis 的分 xml 文件 或者分 类来加载 sql
+*/
+let BLUEWATER_DEFS, dbConnConfig, useCache, dbName, methodQuery;
+
+// 这个函数预读入数据库的配置信息
+let db = (function (readFileSync) {
+
+	// 这里是读入 bluewater 的所有 sql 配置
+	BLUEWATER_DEFS = JSON.parse(readFileSync(process.cwd() + "/res/json/sql.json"), "utf-8");
+	// 这里是数据库的配置，包括数据库类型、数据库连接、用户名密码等
+	// 但 bluewater 不负责实现对这些内容的解析，数据库该怎么连，交给每种数据库独立完成
+	let database = JSON.parse(readFileSync(process.cwd() + "/res/json/bluewater.json"), "utf-8");
+	useCache = database.cache;
+	dbConnConfig = database.connection;
+	dbName = database["database-name"];
+	methodQuery = database["method-query"];
+
+	// 数据库驱动入口
+	return require("./adapter/" + dbName);
+})(require("fs").readFileSync);
+
+// 数据库中间驱动可以使用的数据方法，即实现 bluewater 接口所需要实现的方法
+const OPERTATING_METHODS = ["insert", "delete", "update", "select"
+	// TODO bluewater 暂时不支持所定义的 SQL 操作：
+	// trigger,
+	// procedure,
+	// "function"
+];
+
+function sqlError(msg) {
+	throw new Error(msg || "SQL 错误");
+}
+
+async function queryFunction(queryName, paras, conn) {
+
+	let { method, sql, timeout, condition } = BLUEWATER_DEFS[queryName];
+
+	method = String.trim(method.toLowerCase());
+	timeout = timeout || 0;
+
+	if (!Array.has(OPERTATING_METHODS, method)) sqlError(`bluewater 暂时不支持${method}。`);
+
+	let sqlArgs = {
+		from: paras,
+		condition: condition
+	};
+
+	let [_sql, sqlPara] = db.compire(sql, sqlArgs);
+	let stmt = conn.prepare(_sql); // 实例化 stmt 并获得执行方法
+
+	async function closeStmt() {
+		if (stmt) {
+			await stmt.close();
+			stmt = null;
+		}
+	}
+
+	try {
+		if (!stmt) sqlError("无法建立起目标数据库的连接。 ");
+		if (useCache && method === 'select') {
+			// 当处理类型为 select 且判断使用 cache 的时候，先从缓存中查找是否有已有已被缓存的对象
+			let result = db.getCache(_sql, sqlPara);
+			if (result !== null) {
+				Coralian.logger.log(queryName + " data get from cache.");
+				return result;
+			}
+		} else {
+			db.clearCache(); // 非 select 或 不使用 cache 操作的时候，清空缓存
+		}
+
+		let stmtMethod = stmt[method];
+
+		if (!stmtMethod) sqlError(`${dbName} 暂时不支持所定义的 ${method} 操作： `);
+
+		// 执行数据库
+		Coralian.logger.log(queryName + " start # " + method);
+		Coralian.logger.log(_sql);
+		Coralian.logger.log("args : " + JSON.stringify(sqlPara));
+
+		let result = await stmtMethod(sqlPara);
+		if (method === 'select') {
+			result = db.getRecordsList(result);
+		}
+
+		if (useCache && method === 'select') { // 把搜索到的结果集放到缓存中
+			db.putCache(_sql, sqlPara, result, timeout);
+		}
+
+		return result;
+	} finally {
+		await closeStmt();
+	}
+}
+
+/*
+ * 主函数
+ * 
+ * 通过 let db = bluewater(); 来创建 connetion 的实例
+ * 外部调用不用知道内部逻辑，也不用连接 statement(默认都是 prepare 级的)
+ * 具体的如何实现prepare 则交给各个数据库的驱动自己来实现
+ * 在 connetcion 创建之后，只要通过 其实例调用在 sql.json 中定义好的方法就可以了。
+ * 
+ * 例：
+ * let db = bluewater();
+ * db.getList({
+ *     success : [Function],
+ *     fail : [Function],
+ *     paras : {}
+ * });
+ * 
+ * success : 数据库执行成功后的回调函数（后处理）
+ * fail : 数据库执行失败情况下的处理，如果不提供，bluewater 会有一个默认函数来执行，但默认函数只会将错误记录，而不会去解决错误
+ * paras : 执行 该sql 所需要的参数
+ */
+function bluewater() {
+
+	let conn = db.connect(dbConnConfig);
+
+	async function closeConn() {
+		if (conn) {
+			await conn.close();
+			conn = null;
+		}
+	}
+
+	async function runQueryFunction(queryName, arg) {
+		try {
+			let result = await queryFunction(queryName, arg.condition, conn);
+			arg.success(result);
+		} catch (e) {
+			Coralian.logger.err(e);
+			if (arg.failed) {
+				arg.fail();
+			}
+		} finally {
+			await closeConn();
+		}
+	}
+
+	let bwObj = {
+		// 执行单条sql
+		query: async (arg) => {
+			await runQueryFunction(arg.name, arg);
+		},
+		// 专门用于事务（多条sql的有序执行）
+		transaction: async (queue, success, failed) => {
+			try {
+				await conn.begin();
+				let results = {};
+				while ((item = queue.shift()) != undefined) {
+					let result = await queryFunction(item.name, item.condition, conn);
+					if (item.success) {
+						let ret = results[item.name];
+						if (!ret) {
+							results[item.name] = result;
+						} else {
+							if (typeIs(ret, 'array')) {
+								results[item.name] = ret.concat(result);
+							} else {
+								results[item.name] = [ret];
+								results[item.name].push(result);
+							}
+						
+						}
+						item.success(result);
+					}
+				}
+				await conn.commit();
+				if (success) success(results);
+			} catch (e) {
+				await conn.rollback();
+				Coralian.logger.err(e);
+				e.code = 500;
+				if (failed) failed(e);
+			} finally {
+				await closeConn();
+			}
+		},
+		// 专门用于无需开启事物且无中间操作的多条sql的 有序执行
+		execute: async (queue, success, failed) => {
+			try {
+				let results = {};
+				while ((item = queue.shift()) != undefined) {
+					let result = await queryFunction(item.name, item.condition, conn);
+					let ret = results[item.name];
+					if (!ret) {
+						results[item.name] = result;
+					} else {
+						if (typeIs(ret, 'array')) {
+							results[item.name] = ret.concat(result);
+						} else {
+							results[item.name] = [ret];
+							results[item.name].push(result);
+						}
+					
+					}
+				}
+				if (success) success(results);
+			} catch (e) {
+				Coralian.logger.err(e);
+				e.code = 500;
+				if (failed) failed(e);
+			} finally {
+				await closeConn();
+			}
+		}
+	};
+
+	if (methodQuery === 'ON') {
+		for (let queryName in BLUEWATER_DEFS) {
+
+			bwObj[queryName] = async (arg) => {
+				await runQueryFunction(queryName, arg);
+			};
+		}
+	}
+
+	return bwObj;
+}
+
+bluewater.getDatabaseInfo = db.getDatabaseInfo;
+bluewater.getDBSize = db.getDBSize;
+
+module.exports = bluewater;
