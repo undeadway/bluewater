3a4c17838923745f0e69a3c0ca72463ee235b3da
hzwaygc <hzwaygc@gmail.com>
2020-12-30 21:55:29 +0800

    测试

--- test/index.js
+++ test/index.js
@@ -1,37 +1,4 @@
 require("coralian");
 let bluewater = require("../src/index");
 
-let db1 = bluewater();
-
-db1.listBlogClass({
-	success : bg => {
-		console.log(bg);
-	}
-});
-
-db1.updateUserClassic( {
-	success : ret => {
-		console.log(ret);
-	},
-	paras : {
-		bid : 1227539226000
-	}
-});
-
-db1.insertTag({
-	success : ret => {
-		console.log(ret);
-	},
-	paras : {
-		tname : Date.now()
-	}
-});
-
-// db1.transaction([
-// 	{
-// 		name : "",
-// 		paras : {},
-// 		success : () => {}
-// 	}
-// ], err => {});
 
---
+++ test/lamdba.js
@@ -0,0 +1,26 @@
+require("coralian");
+let bluewater = require("../src/index");
+
+bluewater.lambda("test")
+.like("A", 1, "%A")
+.select(["B",  "C", "D"], (res) => {
+    console.log(res);
+});
+
+bluewater.lambda("test")
+.lessthan("A", 20)
+.select(["B",  "C", "D"], (res) => {
+    console.log(res);
+});
+
+bluewater.lambda("test")
+.overthan("C", 20)
+.select(["A",  "C", "D"], (res) => {
+    console.log(res);
+});
+
+bluewater.lambda("test")
+.between("C", 20, 30)
+.select(["A",  "C", "D"], (res) => {
+    console.log(res);
+});
