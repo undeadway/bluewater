2e50be1f158247668d3cbd981a209242d090a7fb
waygc <hzwaygc@gmail.com>
2021-02-12 22:03:07 +0800

    添加归档文件

--- src/util/archive.js
+++ src/util/archive.js
@@ -1,9 +1,16 @@
 /**
  * 归档模块
  * 
- * 这个模块处理什么时候进行归档
- * 以何种形式进行归档
- * 具体哪些内容需要归档则交程序配置
+ * 归档模块通过从配置中读取归档的类型、归档类型、归档时间、
+ * 归档组织方式等来进行归档处理
  */
-module.exports = exports = (dburl, size) => {
+module.exports = exports = {
+	init: (config) => {
+
+		if (!config) return; // 如果没有配置，则直接终止所有的配置
+
+		const archive = {};
+
+		return archive;
+	}
 }
