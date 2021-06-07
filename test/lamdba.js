bb26f864959be7e73090e0f3e190381b1af43bbf
hzwaygc <hzwaygc@gmail.com>
2021-06-07 14:32:15 +0800

    修改 lamdba 测试

--- test/lamdba.js
+++ test/lamdba.js
@@ -1,26 +1,38 @@
 require("coralian");
-const bluewater = require("../src/index");
 
-bluewater.lambda("test")
-.like("A", 1, "%A")
-.select(["B",  "C", "D"], (res) => {
-    console.log(res);
-});
+try {
+    const bluewater = require("../src/index");
 
-bluewater.lambda("test")
-.lessthan("A", 20)
-.select(["B",  "C", "D"], (res) => {
-    console.log(res);
-});
+    const lamdba = bluewater.lambda("test");
 
-bluewater.lambda("test")
-.overthan("C", 20)
-.select(["A",  "C", "D"], (res) => {
-    console.log(res);
-});
+    lamdba
+    .like("A", 1, "%A")
+    .select(["B",  "C", "D"], (res) => {
+        console.log(res);
+    });
 
-bluewater.lambda("test")
-.between("C", 20, 30)
-.select(["A",  "C", "D"], (res) => {
-    console.log(res);
-});
+    lamdba
+    .lessThan("A", 20)
+    .select(["B",  "C", "D"], (res) => {
+        console.log(res);
+    });
+
+    lamdba
+    .overThan("C", 20)
+    .select(["A",  "C", "D"], (res) => {
+        console.log(res);
+    });
+
+    lamdba
+    .between("C", 20, 30)
+    .select(["A",  "C", "D"], (res) => {
+        console.log(res);
+    });
+
+    lamdba
+    .update({"A": 100}, (res) => {
+        console.log(res);
+    })
+} catch (e) {
+    console.log(e);
+}
