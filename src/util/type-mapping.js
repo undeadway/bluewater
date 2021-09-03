ad9b6886a2c83bd309e9545a13e9aca4963bccac
waygc <hzwaygc@gmail.com>
2021-09-03 14:47:43 +0800

    修改引号

--- src/util/type-mapping.js
+++ src/util/type-mapping.js
@@ -6,11 +6,12 @@ module.exports = {
 	format : (input, type) => {
 		// TODO 暂时先把所有字符串加上引号，其他类型之后再处理
 		switch (type) {
-			case 'nchar':
-			case 'varchar':
-			case 'nvarchar':
-			case 'char':
+			case "nchar":
+			case "varchar":
+			case "nvarchar":
+			case "char":
 				return `'${input}'`;
+			default :
 		}
 		return input;
 	}
